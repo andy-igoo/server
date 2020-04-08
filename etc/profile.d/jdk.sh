b880ed10d970a9b85dffbd63fb930d28e6010c70
Dmitry Fedyuk <dfediuk@gmail.com>
2020-04-08 15:11:28 +0600

    https://github.com/vegandthecity/server/issues/6

---
+++ etc/profile.d/jdk.sh
@@ -0,0 +1,8 @@
+# 2020-04-08 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+# 1) "How did I install Oracle Java 13 for Elasticsearch 6 to Debian 10?": https://df.tips/t/1007
+# 2) "What do the scripts in /etc/profile.d do?": https://unix.stackexchange.com/questions/64258
+export DERBY_HOME=/usr/lib/jvm/jdk-13.0.2/db
+export J2REDIR=/usr/lib/jvm/jdk-13.0.2
+export J2SDKDIR=/usr/lib/jvm/jdk-13.0.2
+export JAVA_HOME=/usr/lib/jvm/jdk-13.0.2
+export PATH=$PATH:/usr/lib/jvm/jdk-13.0.2/bin:/usr/lib/jvm/jdk-13.0.2/db/bin
